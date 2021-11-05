3c8a325f8b1104152a274b2b2d92de91c12fdbd4
Ivan Todorov Vasilev <IVAN TODOROV.VASILEV@local.ittcesena.it>
2021-11-05 08:33:47 +0100

    primo commit

---
+++ Vasilev.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CollezioneCD
+{
+    public class Vasilev
+    {
+        private int es;
+    }
+}
